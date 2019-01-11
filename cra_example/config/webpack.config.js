c4b415ff1782216eb89e4d99344bd8e5518bcb55
YaaMe <969519545@qq.com>
2019-01-11 20:24:20 +0900

    chore(ssr): show config

--- cra_example/config/webpack.config.js
+++ cra_example/config/webpack.config.js
@@ -6,6 +6,7 @@ const webpack = require('webpack');
 const resolve = require('resolve');
 const PnpWebpackPlugin = require('pnp-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 const InlineChunkHtmlPlugin = require('react-dev-utils/InlineChunkHtmlPlugin');
 const TerserPlugin = require('terser-webpack-plugin');
@@ -476,6 +477,7 @@ module.exports = function(webpackEnv) {
           {
             inject: true,
             template: paths.appHtml,
+            inlineSource: '.(js|css)$'
           },
           isEnvProduction
             ? {
@@ -495,6 +497,7 @@ module.exports = function(webpackEnv) {
             : undefined
         )
       ),
+      new HtmlWebpackInlineSourcePlugin(HtmlWebpackPlugin),
       // Inlines the webpack runtime script. This script is too small to warrant
       // a network request.
       isEnvProduction &&
